448bb39b95c2a0be2a856e9bc32923b6549d32ba
rjford11 <rjford@rkfassociatesinc.com>
2019-07-10 13:28:45 -0700

    Completed the game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,9 +1,7 @@
-board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-player1 = ''
-player2 = ''
-playing = True
-
-print('Welcome to Tic Tac Toe!')
+#board = [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+#player1 = ''
+#player2 = ''
+#playing = True
 
 def replay():
     keep_playing = input('Do you wish to keep playing?')
@@ -13,7 +11,7 @@ def full_board_check(board):
     return ' ' in board
     
 def space_check(board, position):
-  return (board[position] == 'X' or board[position] == 'O')
+    return (board[position] != 'X' and board[position] != 'O')
 
 import random
 def choose_first():
@@ -31,32 +29,53 @@ def win_check(board, marker):
      or (marker in board[3] and marker in board[6] and marker in board[9])
      or (marker in board[4] and marker in board[5] and marker in board[6])
      or (marker in board[7] and marker in board[8] and marker in board[9])):
-        print('GAME won by {}'.format(marker))
-        return True
+        print('Congratulations! GAME won by {}'.format(marker))
+        playing = input('Would you like to play again?').lower()
+        if (playing == 'yes'):
+            board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+            print_board(board)
+            player_choice(board, marker)
+        else:
+            return False
     else:
-        return False
+        return True
     
 def place_marker(board, marker, position):
     if (marker.upper() == 'X' or marker.upper() == 'O'):
         if (position < 1 or position > 9):
-           print("Position must be between 1 and 9")
+            print("Position must be between 1 and 9")
         board[position] = marker
+        return board
     else:
         print('Marker must be X or O')
         
-
 def check_position(position):
     if (board.count(position)) == 0:
         board[position] = position
     else:
         return 'fail'
     
-def player_choice(board):
+def player_choice(board, marker):
     playing = True
     while playing:
-        position = int(input("Choose your next position: (1-9"))
-        playing = space_check(board,position)
-        
+        position = int(input("Choose your next position: (1-9)"))
+        if (position < 1 or position > 9):
+            print('The position must be between 1 and 9')
+
+        check = space_check(board,position)
+        if (check == False):
+            print('SPACE Check playing {}'.format(check))
+            print('This space is already taken.  Please choose a different space.')
+            continue
+        board = place_marker(board, marker, position)
+        print_board(board)
+        playing = win_check(board, marker)
+        print('Playing = {}'.format(playing))
+        if (marker == 'X'):
+            marker = 'O'
+        else:
+            marker = 'X'
+
 def player_input():
     while True:
         player1 = input("Player 1:  Do you want to be X or O?")
@@ -72,22 +91,20 @@ def player_input():
     print('Are you ready to play? Enter Yes or No.')
     playing = input().lower()
     if playing == 'yes':
-        board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        board = ['#',' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        print()
         print()
         print_board(board)
+        player_choice(board, player1)
     else:
         print("Why don't you want to play?")
         
 from IPython.display import clear_output
 def print_board(board):
-    #mylist = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
-    #mylist[num-1] = player
-    #if init != 'yes':
-     #   board[num] = player
+    clear_output()
     line  = '   |   |\n'
     line2 = '  | '
     line3 = ' | '
-    
     #patterns = {0:line +mylist[0] +line2 +mylist[1]+ line3 +mylist[2]+'\n'+line +'-----------\n' +line +mylist[3]+line2 +mylist[4] +line3 +mylist[5]+'\n' \
     #            +line +'-----------\n' +line +mylist[6] +line2 +mylist[7] +line3 +mylist[8] +'\n'+line}
     
@@ -96,3 +113,6 @@ def print_board(board):
     
     for pattern in patterns:
         print(patterns[pattern])
+    
+print('Welcome to Tic Tac Toe!')
+player_input()
